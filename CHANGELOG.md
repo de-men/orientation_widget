c5eeec899bf4c85d534444dfdff6170901d47557
Thai <thaihuynhxyz@gmail.com>
2023-11-30 19:40:27 +0700

    1 making transition smooth (#2)
    * feat: only accept 1 OrientationWidget in the tree
    
    * chore: update SDK constraint

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## 1.1.0
+
+* Throw error when use this OrientationWidget in descendants.
+* Not default set auto orientations when dispose.
+
 ## 1.0.0
 
 * First release
--- lib/orientation_widget.dart
+++ lib/orientation_widget.dart
@@ -1,7 +1,7 @@
 library orientation_widget;
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 
 const autoOrientations = <DeviceOrientation>[
   DeviceOrientation.landscapeRight,
@@ -26,6 +26,24 @@ class OrientationWidget extends StatefulWidget {
   final List<DeviceOrientation> orientations;
   final List<DeviceOrientation>? then;
 
+  /// OrientationWidget.portrait is a widget that sets the orientation of the device to portrait.
+  /// Should be used with [MaterialPageRoute] to set the orientation of the page.
+  ///
+  /// [then] is called when the widget is disposed if not null.
+  /// It is useful to restore the orientation to the previous one.
+  /// For example, if you want to restore the orientation to portrait
+  /// after the user leaves the landscape page.
+  ///
+  /// ```dart
+  /// Navigator.of(context).push(
+  ///   MaterialPageRoute(
+  ///     builder: (context) => OrientationWidget.portrait(
+  ///       then: landscapeOrientations,
+  ///       child: const LandscapePage(),
+  ///     ),
+  ///   ),
+  /// );
+  /// ```
   factory OrientationWidget.portrait({
     required Widget child,
     List<DeviceOrientation>? then,
@@ -37,6 +55,24 @@ class OrientationWidget extends StatefulWidget {
     );
   }
 
+  /// OrientationWidget.landscape is a widget that sets the orientation of the device to landscape.
+  /// Should be used with [MaterialPageRoute] to set the orientation of the page.
+  ///
+  /// [then] is called when the widget is disposed if not null.
+  /// It is useful to restore the orientation to the previous one.
+  /// For example, if you want to restore the orientation to portrait
+  /// after the user leaves the landscape page.
+  ///
+  /// ```dart
+  /// Navigator.of(context).push(
+  ///   MaterialPageRoute(
+  ///     builder: (context) => OrientationWidget.landscape(
+  ///       then: portraitOrientations,
+  ///       child: const LandscapePage(),
+  ///     ),
+  ///   ),
+  /// );
+  /// ```
   factory OrientationWidget.landscape({
     required Widget child,
     List<DeviceOrientation>? then,
@@ -48,6 +84,25 @@ class OrientationWidget extends StatefulWidget {
     );
   }
 
+  /// OrientationWidget is a widget that sets the orientation of the device.
+  /// Should be used with [MaterialPageRoute] to set the orientation of the page.
+  ///
+  /// [then] is called when the widget is disposed if not null.
+  /// It is useful to restore the orientation to the previous one.
+  /// For example, if you want to restore the orientation to portrait
+  /// after the user leaves the landscape page.
+  ///
+  /// ```dart
+  /// Navigator.of(context).push(
+  ///   MaterialPageRoute(
+  ///     builder: (context) => const OrientationWidget(
+  ///       orientations: [DeviceOrientation.landscapeLeft],
+  ///       then: portraitOrientations,
+  ///       child: const PortraitPage(),
+  ///     ),
+  ///   ),
+  /// );
+  /// ```
   const OrientationWidget({
     required this.child,
     required this.orientations,
@@ -62,23 +117,30 @@ class OrientationWidget extends StatefulWidget {
 class OrientationWidgetState extends State<OrientationWidget> {
   @override
   void initState() {
-    final state = context.findAncestorStateOfType<OrientationWidgetState>();
-    if (state == null) {
-      SystemChrome.setPreferredOrientations(widget.orientations);
-    } else {
-      debugPrint('Already there is an ancestor orientation widget!');
+    if (context.findAncestorStateOfType<OrientationWidgetState>() != null) {
+      throw StateError('There is another OrientationWidgetState in the tree.');
     }
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return widget.child;
+    return FutureBuilder(
+      future: SystemChrome.setPreferredOrientations(widget.orientations),
+      builder: (_, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return widget.child;
+        }
+        return const SizedBox();
+      },
+    );
   }
 
   @override
   void dispose() {
-    SystemChrome.setPreferredOrientations(widget.then ?? autoOrientations);
+    if (widget.then != null) {
+      SystemChrome.setPreferredOrientations(widget.then!);
+    }
     super.dispose();
   }
 }
